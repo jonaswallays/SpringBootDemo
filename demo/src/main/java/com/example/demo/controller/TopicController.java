9b31f739b32f0f3155ee33b57910ca7711a349c9
Jonas <Jonas@192.168.0.108>
2020-01-25 18:42:38 +0100

    Get single topic

--- demo/src/main/java/com/example/demo/controller/TopicController.java
+++ demo/src/main/java/com/example/demo/controller/TopicController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +19,11 @@ public class TopicController {
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics(){
 		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
 	}
 	
 }
